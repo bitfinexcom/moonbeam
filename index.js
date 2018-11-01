699dd4e3d7e5b1cf7c4300396e0111156287a598
Robert Kowalski <rok@kowalski.gd>
2018-11-01 19:41:30 +0100

    add pluggable adapters
    this adds the possibility to load plugins with the server.
    
    if configured, the mongodb storage adapter will create indexes
    and return the currently used collection, because thats a
    pretty common use case.

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 
 const conf = require('./config/moonbeam.conf.json')
 const server = require('./lib/moonbeam')
-const inst = server(conf)
+
+const dbConf = require('./config/moonbeam.mongo.conf.json')
+const db = require('./lib/mongo')(dbConf)
+const plugins = [{ name: 'db', plugin: db }]
+
+const inst = server(conf, plugins)
+inst.listen((err) => {
+  if (err) throw err
+})
 
 module.exports = inst
