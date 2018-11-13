e5964fb5c9cddd86dcdd5bb6662b20b845fbb882
Robert Kowalski <rok@kowalski.gd>
2018-11-13 10:06:30 +0100

    use mongodb plugin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const conf = require('./config/moonbeam.conf.json')
 const server = require('./lib/moonbeam')
 
 const dbConf = require('./config/moonbeam.mongo.conf.json')
-const db = require('./lib/mongo')(dbConf)
+const db = require('moonbeam-mongodb')(dbConf)
 const plugins = [{ name: 'db', plugin: db }]
 
 const inst = server(conf, plugins)
