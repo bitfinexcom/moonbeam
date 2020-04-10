b8c817f8b1312e0e2b6b50caeea44384c3884532
batrudinych <denis.fatkhudinov@gmail.com>
2020-04-10 17:25:55 +0300

    implemented endpoints
    updates packages

---
+++ config/default.js
@@ -0,0 +1,34 @@
+module.exports = {
+  port: 8181,
+  socket: null,
+
+  maxResults: 1000,
+
+  cors: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'x-csrf-token, Content-Type, Authorization, Content-Length, X-Requested-With'
+  },
+
+  tosCurrent: 1,
+  tosCurrentDate: '2019-07-24',
+  tosAvailable: [1],
+
+  nodeHttpUrl: 'http://INSERT_MAINNET_HTTP_ENDPOINT',
+  contract: '__ACCOUNT__',
+
+  cosign: {
+    pKey: '__EXCHANGE_ACC_PRIVKEY__'
+  },
+
+  hcaptcha: {
+    enabled: false,
+    endpoint: 'https://hcaptcha.com/siteverify',
+    secret: '__SECRET__'
+  },
+
+  dbUsers: {
+    dbName: 'trades',
+    mongoUrl: 'mongodb://localhost',
+    collectionTrades: 'userdata'
+  }
+}
--- worker.js
+++ worker.js
@@ -1,34 +1,8 @@
 'use strict'
 
-const argv = require('yargs').argv
+const { start } = require('./lib/server')
 
-const {
-  moonbeamConf,
-  dbConfUsers,
-  dbConfPublicTrades
-} = require('./lib/helpers/config')
-
-Object.assign(moonbeamConf, argv)
-
-const server = require('./lib/moonbeam')
-
-const dbUsers = require('moonbeam-mongodb')(dbConfUsers)
-const dbPublicTrades = require('moonbeam-mongodb')(dbConfPublicTrades)
-
-const grenacheService = require('./lib/helpers/grenache-service')
-
-const plugins = [
-  { name: 'userDb', plugin: dbUsers },
-  { name: 'publicTradesDb', plugin: dbPublicTrades },
-  { name: 'grenacheService', plugin: grenacheService }
-]
-
-const inst = server(moonbeamConf, plugins)
-inst.listen((err) => {
-  if (err) throw err
-  inst.connect((err) => {
-    if (err) throw err
-  })
+start().catch(err => {
+  console.error(err)
+  process.exit(1)
 })
-
-module.exports = inst
